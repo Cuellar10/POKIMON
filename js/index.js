808bc2cc7a7f1a3103a010a58c63b1b23f03f998
Andrés <tafurandres2816@gmail.com>
2025-03-25 15:28:15 -0500

    Terce cargue

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function Loguear (){
+  let username = document.getElementById('Username').value
+
+    let password = document.getElementById('Clave').value
+        if(username='Andres' && password =='123456'){                                                                
+            alert('Usuario válido')
+        }else{  
+            alert('Usuario incorrecto')
+        }   
+}
